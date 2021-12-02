89851d8af4461cc43e50a3a734b02b3f4ef29a9c
Shagun Agrawal <51134776+standmarsh@users.noreply.github.com>
2021-12-02 23:44:59 +0530

    website setup

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 # Import Section
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS
-from transformers import AutoTokenizer, TFAutoModelForQuestionAnswering
-import tensorflow as tf
-from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+# from transformers import AutoTokenizer, TFAutoModelForQuestionAnswering
+# import tensorflow as tf
+# from sentence_transformers import SentenceTransformer
+# from sklearn.metrics.pairwise import cosine_similarity
 
 # Initializing Models
-tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
-model = TFAutoModelForQuestionAnswering.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
-model_sim = SentenceTransformer('bert-base-nli-mean-tokens')
+# tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+# model = TFAutoModelForQuestionAnswering.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+# model_sim = SentenceTransformer('bert-base-nli-mean-tokens')
 
 # Sample Text
 text = r"""
@@ -19,7 +19,6 @@ World War II or the Second World War, often abbreviated as WWII or WW2, starting
 questions = [
     "Time period of world war 2?",
     "Which alliances were a part of world war 2"
-
 ]
 
 correct_answers = [
@@ -29,36 +28,36 @@ correct_answers = [
 
 generated_answers = []
 
-def generate_answer(question):
-    inputs = tokenizer(question, text, add_special_tokens=True, return_tensors="tf")
-    input_ids = inputs["input_ids"].numpy()[0]
-    outputs = model(inputs)
-    answer_start_scores = outputs.start_logits
-    answer_end_scores = outputs.end_logits
-    answer_start = tf.argmax(answer_start_scores, axis=1).numpy()[0]
-    answer_end = tf.argmax(answer_end_scores, axis=1).numpy()[0] + 1
-    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-    return answer
+# def generate_answer(question):
+#     inputs = tokenizer(question, text, add_special_tokens=True, return_tensors="tf")
+#     input_ids = inputs["input_ids"].numpy()[0]
+#     outputs = model(inputs)
+#     answer_start_scores = outputs.start_logits
+#     answer_end_scores = outputs.end_logits
+#     answer_start = tf.argmax(answer_start_scores, axis=1).numpy()[0]
+#     answer_end = tf.argmax(answer_end_scores, axis=1).numpy()[0] + 1
+#     answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+#     return answer
 
-for question in questions:
-    generated_answers.append(generate_answer(question))
+# for question in questions:
+#     generated_answers.append(generate_answer(question))
 
-def get_similarity(gen_answer, correct_answer):
-    sentences = []
-    sentences.append(correct_answer)
-    sentences.append(gen_answer)
-    sentence_embeddings = model_sim.encode(sentences)
-    arr = cosine_similarity([sentence_embeddings[0]],sentence_embeddings[1:])
-    return arr[0]
+# def get_similarity(gen_answer, correct_answer):
+#     sentences = []
+#     sentences.append(correct_answer)
+#     sentences.append(gen_answer)
+#     sentence_embeddings = model_sim.encode(sentences)
+#     arr = cosine_similarity([sentence_embeddings[0]],sentence_embeddings[1:])
+#     return arr[0]
 
 
-for i in range(0,len(correct_answers)):
-    cur = get_similarity(correct_answers[i], generated_answers[i])
-    print("correct => ", correct_answers[i]," generated => ", generated_answers[i])
-    print(cur)
+# for i in range(0,len(correct_answers)):
+#     cur = get_similarity(correct_answers[i], generated_answers[i])
+#     print("correct => ", correct_answers[i]," generated => ", generated_answers[i])
+#     print(cur)
 
-# app = Flask(__name__)
-# CORS(app)
+app = Flask(__name__)
+CORS(app)
 
 
 # NLP Tasks
@@ -67,13 +66,13 @@ for i in range(0,len(correct_answers)):
 
 # Flask Routes
 
-# @app.route('/')
-# def hello_world():
-#     return 'Works !'
+@app.route('/')
+def hello_world():
+    return 'Works !'
 
 
-# @app.route('/results', methods=["POST"])
-# def get_results():
-#     req_body = request.get_json(force=True)
-#     print(req_body)
-#     return jsonify(req_body)
+@app.route('/results', methods=["POST"])
+def get_results():
+    req_body = request.get_json(force=True)
+    print(req_body)
+    return jsonify(req_body)
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,25 +2,36 @@ import React, { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import axios from "axios";
 import styles from "../styles/Home.module.css";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 
 const Home: NextPage = () => {
   const [questions, setQuestions] = useState<string[]>([]);
+  const [text, setText] = useState<string>("");
 
   useEffect(() => {
     async function getData() {
       const res = await axios.post("http://127.0.0.1:5000/results", {
         hello: "hi",
       });
-      console.log(res);
+      console.log(res.data);
     }
 
     getData();
   }, []);
 
+  const handleChange = (textData: string) => {
+    setText(textData);
+    console.log(textData);
+  };
+
   return (
     <div className={styles.container}>
-      <Button variant="contained">Contained</Button>
+      <TextField
+        multiline
+        variant="outlined"
+        value={text}
+        onChange={(e) => handleChange(e.target.value)}
+      />
     </div>
   );
 };
